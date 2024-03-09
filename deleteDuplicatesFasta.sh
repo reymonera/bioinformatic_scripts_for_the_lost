385f451deb9f64c3f98355f25cc8c4a997909eee
reymonera <castillo.vilcahuaman@gmail.com>
2024-03-09 17:48:18 -0500

    Added seqkit to the mix

--- deleteDuplicatesFasta.sh
+++ deleteDuplicatesFasta.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
 
-# Directorio donde se encuentran los archivos multifasta
-directorio="/ruta/a/tu/carpeta"
+# Specify the directory containing your multifasta files
+fasta_dir="/path/to/your/multifasta/files"
 
-# Recorre todos los archivos multifasta en el directorio
-for archivo in "$directorio"/*.fasta
-do
-    echo "Procesando archivo $archivo"
-    
-    # Crea un array asociativo para almacenar las secuencias
-    declare -A secuencias
+# Create a directory for all the old files
+mkdir duplicated
 
-    # Lee el archivo línea por línea
-    while IFS= read -r linea
-    do
-        # Si la línea comienza con '>', es una cabecera de secuencia
-        if [[ $linea =~ ^\> ]]
-        then
-            cabecera=$linea
-        else
-            # Si la secuencia ya existe en el array, la elimina
-            if [[ ${secuencias[$linea]} ]]
-            then
-                unset secuencias[$cabecera]
-            else
-                secuencias[$cabecera]=$linea
-            fi
-        fi
-    done < "$archivo"
-
-    # Escribe las secuencias no duplicadas a un nuevo archivo
-    for cabecera in "${!secuencias[@]}"
-    do
-        echo "$cabecera" >> "${archivo%.fasta}_nodup.fasta"
-        echo "${secuencias[$cabecera]}" >> "${archivo%.fasta}_nodup.fasta"
-    done
-
-    # Limpia el array para el próximo archivo
-    unset secuencias
+# Loop through each multifasta file in the directory
+for fasta_file in "$fasta_dir"/*.fasta; do
+    # Identify duplicate headers and remove them using seqkit
+    seqkit rmdup -s < "$fasta_file" > "${fasta_file%.fasta}_nodup.fasta"
+    mv "$fasta_file" duplicated/
 done
+
+echo "Duplicate headers removed from all multifasta files."
+
