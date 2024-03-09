8ada45b1e974b2061ec7b21e4b736fa8066ec4ec
reymonera <castillo.vilcahuaman@gmail.com>
2024-03-09 18:34:07 -0500

    added parameter to create duplicated details

--- deleteDuplicatesFasta.sh
+++ deleteDuplicatesFasta.sh
@@ -9,8 +9,9 @@ mkdir duplicated
 # Loop through each multifasta file in the directory
 for fasta_file in "$fasta_dir"/*.fasta; do
     # Identify duplicate headers and remove them using seqkit
-    seqkit rmdup -s < "$fasta_file" > "${fasta_file%.fasta}_nodup.fasta"
+    seqkit rmdup -s "$fasta_file" -o "${fasta_file%.fasta}_nodup.fasta" -D "${fasta_file%.fasta}_duplicated.details"
     mv "$fasta_file" duplicated/
+    mv duplicated.details duplicated/
 done
 
 echo "Duplicate headers removed from all multifasta files."
